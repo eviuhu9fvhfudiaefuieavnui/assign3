b98fabe57022f6db03aa584773fc20c057bc5e24
Albab Siddiqi <2681767@schoolsnet.act.edu.au>
2026-05-13 10:12:42 +1000

    i just added another button

--- assign3/Form1.Designer.cs
+++ assign3/Form1.Designer.cs
@@ -28,21 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.hmbutton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // hmbutton
+            // 
+            this.hmbutton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.hmbutton.Location = new System.Drawing.Point(21, 12);
+            this.hmbutton.Name = "hmbutton";
+            this.hmbutton.Size = new System.Drawing.Size(100, 52);
+            this.hmbutton.TabIndex = 0;
+            this.hmbutton.Text = "Home";
+            this.hmbutton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(150, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Inventory";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hmbutton);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Home";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button hmbutton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- assign3/Inventoryserive.cs
+++ assign3/Inventoryserive.cs
@@ -10,24 +10,14 @@ using System.Windows.Forms;
 
 namespace assign3
 {
-    public partial class Form1 : Form
+    public partial class Inventoryserive : Form
     {
-        public Form1()
+        public Inventoryserive()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void companyname_Click(object sender, EventArgs e)
+        private void Inventoryserive_Load(object sender, EventArgs e)
         {
 
         }
