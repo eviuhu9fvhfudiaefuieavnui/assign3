d9d7102ed437001888931e0291cadd799807072a
Albab Siddiqi <2681767@schoolsnet.act.edu.au>
2026-05-19 19:45:34 +1000

    yooyoy

--- assign3/UC_Home.Designer.cs
+++ assign3/UC_Home.Designer.cs
@@ -28,18 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTitle.Location = new System.Drawing.Point(605, 56);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(237, 32);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Albab Shoe Store";
             // 
             // UC_Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblTitle);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "UC_Home";
-            this.Size = new System.Drawing.Size(750, 450);
+            this.Size = new System.Drawing.Size(2000, 1073);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
     }
 }
